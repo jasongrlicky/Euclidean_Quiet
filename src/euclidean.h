7f8bb225f2cff59b651b3901a446837e2e9e8a06
Jason Grlicky <jason@paracosm.us>
2025-01-12 09:58:39 -0800

    [organize] Move euclid fn to its own file

---
+++ src/euclidean.h
@@ -0,0 +1,11 @@
+#ifndef EUCLIDEAN_H_
+#define EUCLIDEAN_H_
+
+#include <stdint.h>
+
+unsigned int euclid(int n, int k, int o);
+uint16_t rightRotate(int shift, uint16_t value, uint8_t pattern_length);
+int findlength(unsigned int bnry);
+unsigned int ConcatBin(unsigned int bina, unsigned int binb);
+
+#endif /* EUCLIDEAN_H_ */ 
