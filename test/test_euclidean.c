ba97557842777fbaa1c35da7ec40e8f4e5b393d2
Jason Grlicky <jason@paracosm.us>
2025-01-12 16:59:54 -0800

    [test] Added tests for euclidean fn

--- test/test_euclidean.c
+++ test/test_euclidean.c
@@ -16,6 +16,11 @@ void test_zero_length(void) {
     }
 }
 
+void test_length_one(void) {
+    TEST_ASSERT_EQUAL_UINT16(0, euclid(1, 0, 0));
+    TEST_ASSERT_EQUAL_UINT16(1, euclid(1, 1, 0));
+}
+
 void test_zero_density(void) {
     for (uint8_t length = 0; length < 16; length++) {
         for (uint8_t offset = 0; offset < 16; offset++) {
@@ -32,12 +37,43 @@ void test_max_density(void) {
     }
 }
 
+void test_offset(void) {
+    for (uint8_t offset = 0; offset < 16; offset++) {
+        uint16_t expected = 0x01 << (15 - offset);
+        TEST_ASSERT_EQUAL_UINT16(expected, euclid(16, 1, offset));
+    }
+}
+
+void test_pattern_gen_smoke(void) {
+    // Just a bunch of patterns
+    TEST_ASSERT_EQUAL_UINT16(0b1010, euclid(4, 2, 0));
+    TEST_ASSERT_EQUAL_UINT16(0b0101, euclid(4, 2, 1));
+    TEST_ASSERT_EQUAL_UINT16(0b1111100, euclid(7, 5, 5));
+    TEST_ASSERT_EQUAL_UINT16(0b01001001, euclid(8, 3, 1));
+    TEST_ASSERT_EQUAL_UINT16(0b10100100, euclid(8, 3, 2));
+    TEST_ASSERT_EQUAL_UINT16(0b0100101001001, euclid(13, 5, 9));
+    TEST_ASSERT_EQUAL_UINT16(0b1110111111111111, euclid(16, 15, 4));
+
+    // 16 rotations of every other beat with a rhythm of 16 steps
+    for (uint8_t offset = 0; offset < 16; offset++) {
+        uint16_t r = euclid(16, 8, offset);
+        if ((offset % 2) == 0) {
+            TEST_ASSERT_EQUAL_UINT16(0b1010101010101010, r);
+        } else {
+            TEST_ASSERT_EQUAL_UINT16(0b0101010101010101, r);
+        }
+    }
+}
+
 int main( int argc, char **argv) {
     UNITY_BEGIN();
 
     RUN_TEST(test_zero_length);
+    RUN_TEST(test_length_one);
     RUN_TEST(test_zero_density);
     RUN_TEST(test_max_density);
+    RUN_TEST(test_offset);
+    RUN_TEST(test_pattern_gen_smoke);
 
     UNITY_END();
 }
