10cceec669092aeaa0b35693546793cfc185b264
Jason Grlicky <jason@paracosm.us>
2025-01-12 16:13:41 -0800

    [test] Added first two tests for euclidean algorithm

---
+++ test/test_euclidean.c
@@ -0,0 +1,34 @@
+#include <unity.h>
+
+#include <euclidean.h>
+
+// required on Windows
+void setUp(void) { }
+
+// required on Windows
+void tearDown(void) { }
+
+void test_zero_length(void) {
+    for (uint8_t density = 0; density < 16; density++) {
+        for (uint8_t offset = 0; offset < 16; offset++) {
+            TEST_ASSERT_EQUAL_UINT16(0, euclid(0, density, offset));
+        }
+    }
+}
+
+void test_zero_density(void) {
+    for (uint8_t length = 0; length < 16; length++) {
+        for (uint8_t offset = 0; offset < 16; offset++) {
+            TEST_ASSERT_EQUAL_UINT16(0, euclid(length, 0, offset));
+        }
+    }
+}
+
+int main( int argc, char **argv) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_zero_length);
+    RUN_TEST(test_zero_density);
+
+    UNITY_END();
+}
