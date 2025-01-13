efcdd1d4dfa20b796ceb121df059bdb4861f506a
Jason Grlicky <jason@paracosm.us>
2025-01-12 16:31:08 -0800

    [docs] Doc comment on euclid fn

--- lib/euclidean/euclidean.h
+++ lib/euclidean/euclidean.h
@@ -3,7 +3,13 @@
 
 #include <stdint.h>
 
+/// Euclid calculation function
+/// Returns a 16-step Euclidean rhythm in the form of a bit flag (beat flag?)
+/// @param n Length: Number of beats rhythm will take.
+/// @param k Density: Number of beats to be on.
+/// @param o Offset: Rotation of the beat. May not exceed length.
 uint16_t euclid(int n, int k, int o);
+
 uint16_t rightRotate(int shift, uint16_t value, uint8_t pattern_length);
 int findlength(unsigned int bnry);
 unsigned int ConcatBin(unsigned int bina, unsigned int binb);
