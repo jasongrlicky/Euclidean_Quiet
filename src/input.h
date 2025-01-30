4ccc75aba9100e7824e1beedea3dba8ae74d5621
Jason Grlicky <jason@paracosm.us>
2025-01-30 12:06:07 -0800

    [docs] Comment on fns in input module

--- src/input.h
+++ src/input.h
@@ -3,8 +3,19 @@
 
 #include "hardware.h"
 
+/// @brief Detects rising edge for reset input
+/// @param reset_in_value Raw reading from reset input pin
+/// @return `true` if a rising edge was detected, `false` otherwise
 bool input_detect_reset(int reset_in_value);
+
+/// @brief Detects rising edge for trigger input
+/// @param trig_in_value Raw reading from trig input pin
+/// @return `true` if a rising edge was detected, `false` otherwise
 bool input_detect_trig(int trig_in_value);
+
+/// @brief Detects an encoder being pushed
+/// @param channel_switch_val Raw reading from channel switch pin
+/// @return `true` if an encoder was pushed, `false` otherwise
 EncoderIdx input_detect_enc_push(int channel_switch_val);
 
 #endif /* INPUT_H_ */
