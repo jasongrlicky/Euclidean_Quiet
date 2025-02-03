57c0413508958f90860d5a8fb3ac1053010b4c4c
Jason Grlicky <jason@paracosm.us>
2025-02-02 18:38:06 -0800

    Add indicators module

---
+++ src/indicators.c
@@ -0,0 +1,1 @@
+#include "indicators.h"
---
+++ src/indicators.h
@@ -0,0 +1,10 @@
+#ifndef INDICATORS_H_
+#define INDICATORS_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INDICATORS_H_ */
