0b5a8bda9d96e8820c2865376972807ed9c00414
Jason Grlicky <jason@paracosm.us>
2025-02-03 20:23:53 -0800

    [format]

--- src/common/events.c
+++ src/common/events.c
@@ -10,8 +10,13 @@ const InputEvents INPUT_EVENTS_EMPTY = {
 
 // cppcheck-suppress unusedFunction
 bool input_events_contains_any_external(const InputEvents *events) {
-	bool result = (events->trig || events->reset || (events->enc_push != ENCODER_NONE) ||
-	               (events->enc_move[ENCODER_1] != 0) || (events->enc_move[ENCODER_2] != 0) ||
-	               (events->enc_move[ENCODER_3] != 0));
+	// clang-format off
+	bool result = (events->trig || 
+                   events->reset || 
+                  (events->enc_push != ENCODER_NONE) ||
+	              (events->enc_move[ENCODER_1] != 0) || 
+                  (events->enc_move[ENCODER_2] != 0) ||
+	              (events->enc_move[ENCODER_3] != 0));
+	// clang-format on
 	return result;
 }
