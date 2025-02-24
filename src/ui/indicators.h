ff27267bcf9856d6ae1e52f7ee9238646607ef3e
Jason Grlicky <jason@paracosm.us>
2025-02-23 21:15:01 -0800

    [refactor] Pass framebuffer to indicator drawing fns

--- src/ui/indicators.h
+++ src/ui/indicators.h
@@ -6,16 +6,17 @@ extern "C" {
 
 #include "common/events.h"
 #include "common/types.h"
+#include "framebuffer.h"
 
 #include <stdint.h>
 
-void indicators_input_update(const InputEvents *events, Milliseconds now);
+void indicators_input_update(Framebuffer *fb, const InputEvents *events, Milliseconds now);
 
 /// Draw output indicators for latching outputs - ones that stay lit until they
 /// are specifically unlit on the next clock cycle.
-/// @param out_channels_firing Bitflags storing which output channels will fire 
+/// @param out_channels_firing Bitflags storing which output channels will fire
 /// this cycle, indexed by `OutputChannel`.
-void indicators_output_draw_latching(uint8_t out_channels_firing);
+void indicators_output_draw_latching(Framebuffer *fb, uint8_t out_channels_firing);
 
 #ifdef __cplusplus
 }
