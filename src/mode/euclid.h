5810929cbc56edfff2a5cc5a47b96c693a88dde3
Jason Grlicky <jason@paracosm.us>
2025-02-28 18:34:42 -0800

    [cleanup] Removed unnecessary imports

--- src/mode/euclid.h
+++ src/mode/euclid.h
@@ -9,9 +9,6 @@ extern "C" {
 #include "common/types.h"
 #include "ui/framebuffer.h"
 
-#include <stdbool.h>
-#include <stdint.h>
-
 void euclid_params_validate(Params *params);
 void euclid_init(const Params *params, Framebuffer *fb);
 void euclid_update(Params *params, Framebuffer *fb, const InputEvents *events, Milliseconds now);
